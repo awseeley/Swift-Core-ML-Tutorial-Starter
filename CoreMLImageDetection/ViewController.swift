1471568d21c9e17ba6aa63185625a5eb557f100b
Andrew Seeley <awseeley@gmail.com>
2017-06-12 19:42:46 +1000

    Swift Core ML Tutorial Starter
    Swift Core ML Tutorial Starter

---
+++ CoreMLImageDetection/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  CoreMLImageDetection
+//
+//  Created by Andrew Seeley on 12/6/17.
+//  Copyright © 2017 Seemu. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var myPhoto: UIImageView!
+    @IBOutlet var lblResult: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func pickImage(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myPhoto.contentMode = .scaleToFill
+            myPhoto.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
